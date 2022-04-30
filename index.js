4343dc6d59fbbcc4889244135253d72a72144c7b
Graciela Parente <graciparentte@gmail.com>
2022-04-29 23:50:27 -0300

    feat: create database called  api-database on mysql and add connection with the DB

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 //server
 const customExpress = require('./config/customExpress');
+const connection = require('./infrastructure/connection');
 
-const app = customExpress();
-app.listen(3000, () => console.log('launch server on 3000 port'));
+connection.connect((erro) => {
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log('successfully connected');
+        const app = customExpress();
+        app.listen(3000, () => console.log('launch server on 3000 port'));
+    }
+});
